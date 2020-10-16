eb52a09d2c014b1bb7a3981c5cdecd61563fcf35
William White <willcwhite@gmail.com>
2020-10-16 10:41:19 -0400

    documentation

--- connect.py
+++ connect.py
@@ -1,11 +1,13 @@
 import mysql.connector
 from mysql.connector import Error
 
+#Establish connection to MySQL server & DB
+
 try:
     connection = mysql.connector.connect(host='localhost',
                                  database='bamazon',
                                  user='root',
-                                 password='XXXXXXXXX')
+                                 password='XXXXXX')
     if connection.is_connected():
         db_Info = connection.get_server_info()
         print("Connected to MySQL server version ",db_Info)
