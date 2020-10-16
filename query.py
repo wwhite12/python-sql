9d9807af01fafa059b6976f9ec9eab0988c50fcd
William White <willcwhite@gmail.com>
2020-10-16 11:03:45 -0400

    query for data, use loop to print specific data

--- query.py
+++ query.py
@@ -5,13 +5,21 @@ try:
     connection = mysql.connector.connect(host='localhost',
                                          database='bamazon',
                                          user='root',
-                                         password='XXXXX')
+                                         password='XXXX')
     #now connected, go into query
     sqlQuery = "SELECT * FROM Products"
     cursor = connection.cursor()
     cursor.execute(sqlQuery)
     records = cursor.fetchall()
     print("Total rows in table",cursor.rowcount)
+
+    for row in records:
+        print("ID = ", row[0])
+        print("Product name = ",row[1])
+        print("dep_name = ",row[2])
+        print("price = ",row[3])
+        print("stock = ",row[4])
+        
 
 except Error as e:
     print("Error reading MySQL table",e)
