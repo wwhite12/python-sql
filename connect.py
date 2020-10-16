f78d7e565f5514c14ef0ad8b3c0f74987d67145e
William White <willcwhite@gmail.com>
2020-10-16 10:34:55 -0400

    connection to MySQL server and database established

---
+++ connect.py
@@ -0,0 +1,22 @@
+import mysql.connector
+from mysql.connector import Error
+
+try:
+    connection = mysql.connector.connect(host='localhost',
+                                 database='bamazon',
+                                 user='root',
+                                 password='XXXXXXXXX')
+    if connection.is_connected():
+        db_Info = connection.get_server_info()
+        print("Connected to MySQL server version ",db_Info)
+        cursor = connection.cursor()
+        cursor.execute("select database();")
+        record = cursor.fetchone()
+        print("Connected to database ",record)
+except Error as e:
+    print("Error while connecting to MySQL ",e)
+finally:
+    if connection.is_connected():
+        cursor.close()
+        connection.close()
+        print("MySQL connection closed")
